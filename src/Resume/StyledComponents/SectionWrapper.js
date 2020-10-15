a1e54666ce930239d2c02d5f63cdda4b04657b2f
marioperez25 <mariofph@gmail.com>
2020-10-14 20:33:33 -0600

    sectino wrapper adjustments

--- src/Resume/StyledComponents/SectionWrapper.js
+++ src/Resume/StyledComponents/SectionWrapper.js
@@ -1,19 +1,13 @@
 import styled from 'styled-components'
 
 const SectionWrapper = styled.section`
-    padding: 0px 30px;
+    padding: 0px 30px 30px 30px;
     max-width: 600px;
     width: fit-content;
     margin: auto;
-    @media(min-width: 768px){
-        padding: 0px 30px;
-    }
     font-family: 'Cabin', sans-serif;
     &:first-of-type {
         padding: 30px 30px;
-        @media(min-width: 768px){
-            padding: 30px 30px;
-        }
     }
 `;
 
